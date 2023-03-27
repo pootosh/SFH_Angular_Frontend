982a0750063086ca9d43e0aa1b759a44a4cb3374
Aditya Kumar SIngh <adityasingh9501@gmail.com>
2023-03-27 23:00:52 +0530

    Add to cart button adjustment

--- src/app/shared/services/product.service.ts
+++ src/app/shared/services/product.service.ts
@@ -10,7 +10,7 @@ productList: Product[];
 
   constructor() {
     this.productList= [
-      new Product('Aditya', 'Singh', 100, 10, 200),
+      new Product('Paracetamol', 'Paracetamol is a common painkiller and fever-reducing medication used worldwide. It acts by reducing the production of prostaglandins in the brain that are responsible for causing pain and inflammation. Paracetamol is a relatively safe medication when taken in recommended doses, and the potential side effects are usually mild. It is available over-the-counter in most countries and is used to treat a variety of conditions such as headaches, period pain, toothache, joint pains, and flu symptoms. However, it is essential to follow the recommended dosage instructions and avoid taking too much or too frequently to avoid accidental overdose or liver damage. Paracetamol is also widely used in combination with other medications and is commonly found in cough and cold remedies.', 100, 10, 200),
       new Product('Aman', 'Singh', 100, 10, 200),
       new Product('Abhishek', 'Singh', 100, 10, 200),
       new Product('Sahil', 'Yadav', 100, 10, 200),
